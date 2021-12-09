ddcb3e0101d5ec277983574602ae30581f667dbc
filka <filka@tutanota.com>
2021-12-09 17:19:31 +0300

    add chmod +x

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,9 @@
 ls -la /usr/local/etc/rc.d/
 ls -la /usr/local/lib/torr-server
 
+chmod +x /usr/local/etc/rc.d/torr-server
+chmod +x /usr/local/lib/torr-server/TorrServer-freebsd-amd64
+
 export GODEBUG=madvdontneed=1
 
 # Enable the service
